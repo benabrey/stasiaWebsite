5308fe2a6cbe3e625a99d8e860f91a15c63c70b7
Ben Abrey <benabrey1417@gmail.com>
2026-04-24 14:01:53 -0700

    Interval time in hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
       shuffled[current].classList.remove('active');
       current = (current + 1) % shuffled.length;
       shuffled[current].classList.add('active');
-    }, 6000);
+    }, 4000);
   }
 
 // ============================================================
